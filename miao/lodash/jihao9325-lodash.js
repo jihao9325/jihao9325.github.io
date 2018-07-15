e282aae3c2d2b35180254f3b402541320f2da2f1
jihao <478492954@qq.com>
2018-07-15 14:29:14 +0800

    add chunk compact

--- miao/lodash/jihao9325-lodash.js
+++ miao/lodash/jihao9325-lodash.js
@@ -1,3 +1,29 @@
 var jihao9325 = {
-  chunk: function (array, size) {}
+  chunk: function (array, size = 1) {
+    var result = []
+    for (var i = 0; i < array.length; i += size) {
+      result.push(array.slice(i, i + size))
+    }
+    return result
+  },
+
+  compact: function (array) {
+    var result = []
+    for (var i = 0; i < array.length; i++) {
+      if ((array[i] & array[i]) != 0) {
+        result.push(array[i])
+      }
+    }
+    return result
+  },
+
+  
+
+
+
+
+
+
+
+
 }
